7d5cd352b0aa5cb0df6f8da1b464c0041eb73c64
Isabiagomes <92949023+Isabiagomes@users.noreply.github.com>
2022-10-26 20:27:52 -0300

    chore: editado main

--- main.cpp
+++ main.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include <cstring>
+#include "intruso.hpp"
 
-using namespace std;
+int main(){
+    int num_entradas, i;
+    std::string senha;
+    Intruso intruso;
+    
+    std::cin >> num_entradas;
+    std::cin.ignore();
 
-int main()
-{
-    int np[6][11], d1[20], d2[20], n;
-    while (cin >> n && n) {
-        memset(np, 1, sizeof(np));
-        for (int vez = 1; vez <= n; vez++)
-        {
-            for (char c = 'A'; c <= 'E'; c++)
-                cin >> d1[c - 'A'] >> d2[c - 'A'];
-
-            for (int digitodavez = 0; digitodavez < 6; digitodavez++)
-            {
-                char c;
-                cin >> c;
-                for (int novodigito = 0; novodigito <= 9; novodigito++)
-                {
-                    if (novodigito == d1[c - 'A'] ||novodigito == d2[c - 'A'])
-                        continue;
-                    np[digitodavez][novodigito] = 0;
-                }
-            }
-        }
-        
-        for (int digitodavez = 0; digitodavez < 6; digitodavez++)
-            for (int novodigito = 0; novodigito <= 9; novodigito++)
-                if (np[digitodavez][novodigito])
-                    cout << novodigito << " ";
-
-        cout << endl << endl;
+    for(i=0; i < num_entradas; i++){
+        std::getline(std::cin, senha);
+        intruso.set_senha_vazada(senha);
     }
+    std::cout << intruso.crack_senha() <<std::endl;
+    
     return 0;
 }
